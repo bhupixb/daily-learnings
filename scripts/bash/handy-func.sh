999641acd8d5f695eb1c3114b4c2b17dd6c46160
Bhupendra Yadav <30554307+bhupixb@users.noreply.github.com>
2022-05-06 23:22:12 +0530

    Function to print time in different formats.

--- scripts/bash/handy-func.sh
+++ scripts/bash/handy-func.sh
@@ -13,3 +13,14 @@ function how() {
 
     curl "cheat.sh/${tool}/${cheat_sh_format}"
 }
+
+# Print current time in IST, UTC, unix timestamp
+function now () {
+	local yellow='\033[0;33m'
+	local green='\033[0;32m'
+	local NC='\033[0m'
+	echo "${yellow}IST: ${green}$(date)${NC}${NC}"
+	echo "${yellow}UTC: ${green}$(date -u)${NC}${NC}"
+	echo "${yellow}UTC ISO: ${green}$(date -u +"%Y-%m-%dT%H:%M:%SZ")${NC}${NC}"
+	echo "${yellow}UNIX: ${green}$(date +%s)${NC}${NC}"
+}
