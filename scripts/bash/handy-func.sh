170dd894637c663056c5a67327442d888331732d
Bhupendra Yadav <30554307+bhupixb@users.noreply.github.com>
2022-06-24 16:01:27 +0530

    Update now function to print date some day, hr etc ago.

--- scripts/bash/handy-func.sh
+++ scripts/bash/handy-func.sh
@@ -14,13 +14,27 @@ function how() {
     curl "cheat.sh/${tool}/${cheat_sh_format}"
 }
 
-# Print current time in IST, UTC, unix timestamp
-function now () {
-	local yellow='\033[0;33m'
-	local green='\033[0;32m'
-	local NC='\033[0m'
-	echo "${yellow}IST: ${green}$(date)${NC}${NC}"
-	echo "${yellow}UTC: ${green}$(date -u)${NC}${NC}"
-	echo "${yellow}UTC ISO: ${green}$(date -u +"%Y-%m-%dT%H:%M:%SZ")${NC}${NC}"
-	echo "${yellow}UNIX: ${green}$(date +%s)${NC}${NC}"
+# Print date,time in local, UTC and unixtimestamp format
+# Tested on OSX
+function now() {
+    # set -x
+    local yellow='\033[0;33m'
+    local green='\033[0;32m'
+    local NC='\033[0m' # No Color
+    local ago=$1
+    # Print date with give interval substracted
+    # one of either y, m, w, d, H, M or S must be used to specify which part of the date is to be adjusted.
+    if [ ! -z $ago ]; then
+        echo "in"
+        echo "${yellow}IST: ${green}$(date -j -v-${ago}) ${NC}${NC}"
+        echo "${yellow}UTC: ${green}$(date -j -v-${ago} -u)${NC}${NC}"
+        echo "${yellow}UTC ISO: ${green}$(date -j -v-${ago} -u +"%Y-%m-%dT%H:%M:%SZ")${NC}${NC}"
+        echo "${yellow}UNIX: ${green}$(date -j -v-${ago} +%s)${NC}${NC}"
+    # Print current date
+    else
+        echo "${yellow}IST: ${green}${local_date} ${NC}${NC}"
+        echo "${yellow}UTC: ${green}$(date -u)${NC}${NC}"
+        echo "${yellow}UTC ISO: ${green}$(date -u +"%Y-%m-%dT%H:%M:%SZ")${NC}${NC}"
+        echo "${yellow}UNIX: ${green}$(date +%s)${NC}${NC}"
+    fi
 }
